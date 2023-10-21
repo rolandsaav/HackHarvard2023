0c042a3abef25521de04056fffe16db5a1933c7b
Roland Saavedra <rolandsaavedra22@gmail.com>
2023-10-20 23:50:25 -0400

    initial keyboard avoiding view and safe area

--- app/App.js
+++ app/App.js
@@ -1,20 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { KeyboardAvoidingView, SafeAreaView, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <KeyboardAvoidingView style={styles.keeb} behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+      <Text style={{color: "#FFF"}}>Open up App.js to start working on your app!</Text>
+      <TextInput placeholderTextColor={"#FFF"} style={styles.input} placeholder="Useless placeholder"/>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#1d1e21',
   },
+  input: {
+    color: "#FFF",
+    fontSize: 24,
+    width: "100%",
+    borderWidth: 1,
+    borderColor: "#FFF",
+    paddingVertical: 4,
+    paddingHorizontal: 8,
+  },
+  keeb: {
+    height: "100%",
+    borderWidth: 1,
+    borderColor: "#FFF",
+    justifyContent: "flex-end"
+  }
 });
