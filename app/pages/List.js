45a68e33a27c9b09bd75ad2ea7600496a1d50b94
nrogers39 <nrogers39@gatech.edu>
2023-10-22 01:22:57 -0400

    made basic layout for events tab

--- app/pages/List.js
+++ app/pages/List.js
@@ -1,11 +1,26 @@
-import { View } from "react-native"
+import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native'
+import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs';
+import EventInList from '../components/EventInList';
 
 const List = () => {
     return (
-        <View>
-            <Text>List</Text>
-        </View>
+        <SafeAreaView style={[styles.container]}>
+            <ScrollView>
+                <EventInList title="Title text" location="Hell" time="Always"/>
+                <EventInList title="Title text" location="Hell" time="Always"/>
+                <EventInList title="Title text" location="Hell" time="Always"/>
+                <EventInList title="Title text" location="Hell" time="Always"/>
+            </ScrollView>
+        </SafeAreaView>
+
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#e28f66"
+    }
+})
 
 export default List
